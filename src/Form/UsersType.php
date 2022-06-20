adf663cbe524f755de2586dd5d02e48abc158d80
Pape Samba SIDIBE <papesambas@yahoo.fr>
2022-06-20 16:17:39 +0000

    creation du crud USERS

---
+++ src/Form/UsersType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Users;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UsersType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            ->add('roles')
+            ->add('password')
+            ->add('nom')
+            ->add('prenom')
+            ->add('telephone')
+            ->add('email')
+            ->add('isVerified')
+            ->add('isActif')
+            ->add('createdAt')
+            ->add('etablissement')
+            ->add('favoris')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+}
